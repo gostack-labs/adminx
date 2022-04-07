306d5099487d5d2ff42077f1178e86266bbf9807
bytegolang <chenzhenying@88.com>
2022-04-07 10:49:51 +0800

    fix:gin

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello world")
+	r := gin.New()
+
+	r.Use(gin.Logger(), gin.Recovery())
+
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"hello": "world"})
+	})
+
+	r.NoRoute(func(ctx *gin.Context) {
+		acceptStr := ctx.Request.Header.Get("Accept")
+		if strings.Contains(acceptStr, "text/html") {
+			ctx.String(http.StatusNotFound, "页面返回 404")
+		} else {
+			ctx.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "路由未定义，请确认 URL 和请求方法是否正确。"})
+		}
+	})
+
+	r.Run(":9999")
 }
